ee9a5ce6f909c53032b978b2d3e53a66f77886be
bastardsnow <imnirajbind@gmail.com>
2026-01-09 09:20:57 +0530

    Fix missing padding import in GlassPanel.kt

--- app/src/main/java/com/callblockerpro/app/ui/components/GlassPanel.kt
+++ app/src/main/java/com/callblockerpro/app/ui/components/GlassPanel.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
